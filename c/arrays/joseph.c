72daf54eee5fa08834b3b64e712a8ba9985cb02c
matan.chen <matanchen78@gmail.com>
2025-01-14 22:50:25 +0200

    joseph comments

--- c/arrays/joseph.c
+++ c/arrays/joseph.c
@@ -56,8 +56,8 @@ size_t Josephnlogn(size_t size) /* n, n/2, n/4, n/8, n/16  ~=  n*log(n)*/
 /* Reviewed by Korin o(n) */
 size_t Josephn(size_t size)
 {
-	size_t current_index = 0;
-	size_t next_index;
+	size_t current_soldier = 0; /* current_index corresponds to the current soldier */
+	size_t next_soldier;
 	size_t i;
     
 	size_t* arr = (size_t*)malloc(size * sizeof(size_t));
@@ -69,16 +69,15 @@ size_t Josephn(size_t size)
 
     arr[size-1] = 0;
 
-    while(arr[current_index] != current_index)
+    while(arr[current_soldier] != current_soldier)
     {
-        next_index = arr[current_index]; /* next soldier alive is arr[current_index], so thats the next index */ /* who is next soldier alive */
-	arr[current_index] = arr[next_index]; /* change arr[current_index] so that it points to the one that the dying one is pointing to */ /* killing that next soldier */
-	current_index = arr[current_index]; /* make the current_index for the next iteration be that soldier that the dying one pointed to */ /* giving the sword to the following one alive */
-	
+        next_soldier = arr[current_soldier]; /* nearest next soldier alive is in arr[current_index], so thats the next index */ /* who is next soldier alive */
+	    arr[current_soldier] = arr[next_soldier]; /* change arr[current_index] so that it points to the one that the dying one is pointing to */ /* killing that next soldier */
+	    current_soldier = arr[current_soldier]; /* make the current_index for the next iteration be that soldier that the dying one pointed to */ /* giving the sword to the nearest next alive soldier following the one that died */
     }
     
     free(arr);
-    return current_index;
+    return next_soldier;
 }
 
 

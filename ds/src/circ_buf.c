ec153ad89522ea339770e6f042753cde98375d84
matan.chen <matanchen78@gmail.com>
2025-02-06 18:26:40 +0200

    circ update

--- ds/src/circ_buf.c
+++ ds/src/circ_buf.c
@@ -7,21 +7,20 @@
 * Author:
 *   Matan Chen
 *
-* Review History:
+* Review History: Albert
 *   - 
 *
 ******************************************************************************/
-#include <stdio.h>
-#include <sys/types.h>
-#include <assert.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h> /* ssize_t */
+#include <assert.h> /* assert */
+#include <string.h> /* memcpy */
+#include <stdlib.h> /* dynamic memory allocations */
 #include "../include/circ_buf.h"
 
 #define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
 #define HEAD(BUF) (BUF->arr + BUF->head_idx)
 #define HEAD_INDEX(BUF) (BUF->head_idx)
-#define TAIL_INDEX(BUF) ((size_t)(BUF->head_idx + BUF->size) % BUF->capacity)
+#define TAIL_INDEX(BUF) ((BUF->head_idx + BUF->size) % BUF->capacity)
 #define TAIL(BUF) (BUF->arr + TAIL_INDEX(BUF))
 #define MIN(a,b) ((a < b) ? a : b)
 
@@ -43,7 +42,6 @@ circ_buf_t* BufCreate(size_t capacity)
 	buffer->capacity = capacity;
 	return buffer;
 }
-
 
 void BufDestroy(circ_buf_t* buf)
 {
@@ -71,7 +69,6 @@ int BufIsEmpty(const circ_buf_t* buf)
 	return (0 == buf->size);
 }
 
-
 ssize_t BufRead(circ_buf_t* buf, char* dst, size_t n_bytes)
 {
 	size_t bytes_to_wrap = 0;
@@ -89,14 +86,13 @@ ssize_t BufRead(circ_buf_t* buf, char* dst, size_t n_bytes)
 	first_chunk = MIN(bytes_to_read, bytes_to_wrap);
 
 	memcpy(dst, HEAD(buf), first_chunk);
-	HEAD_INDEX(buf) += (first_chunk % buf->capacity);
+	HEAD_INDEX(buf) += first_chunk; /* first_chunk % buf->capacity? */
     if (first_chunk < bytes_to_read) 
     {
     	second_chunk = (bytes_to_read - first_chunk);
         memcpy(dst + first_chunk, buf->arr, second_chunk);
-        HEAD_INDEX(buf) += (second_chunk % buf->capacity);
+        HEAD_INDEX(buf) += second_chunk; /* first_chunk % buf->capacity? */
     }
-
 	buf->size -= bytes_to_read;
 	return bytes_to_read;
 }
@@ -112,7 +108,6 @@ ssize_t BufWrite(circ_buf_t* buf, const char* src, size_t n_bytes)
 	{
 		return (-1);
 	}
-
 	
 	bytes_to_wrap = buf->capacity - TAIL_INDEX(buf);
 	bytes_to_write = MIN(BufFreeSpace(buf), n_bytes);
